41ea48e388670574cb63809c3e99d8a37da5cb23
tresna <tresna312@gmail.com>
2023-09-07 09:06:50 +0600

    19

--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,22 @@ Route::get('/', [DemoController::class, 'Home']);
 Route::group(['prefix'='account'],function(){
 
 Route::get('/profile',function{
+    return "profile";
 
 });
 Route::get('/login',function{
-
+    return "login";
 });
 Route::get('/logout',function{
-
+    return "logout";
 });
 Route::get('/signup',function{
+    return "signup";
 
 });
 Route::get('/updateProfile',function{
-    
+    return "updateProfile";
+
 });
 
 });
