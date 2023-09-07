7cce6d6511326be1b2124c39de01ed330bc0b587
tresna <tresna312@gmail.com>
2023-09-07 09:27:11 +0600

    19

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DemoController;
 
-Route::get('/Name/{firstName}/{middleName}/{lastName}', [DemoController::class, 'MyName']);
-Route::get('/', [DemoController::class, 'Home']);
+//Route::get('/Name/{firstName}/{middleName}/{lastName}', [DemoController::class, 'MyName']);
+//Route::get('/', [DemoController::class, 'Home']);
 
 
 Route::group(['prefix'=>'account'],function(){
